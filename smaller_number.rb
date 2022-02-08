9cf2a46ea0881cfcd72d18ce14edee981bc917cf
NicolaCortese <nicolacortese92@gmail.com>
2022-02-08 12:16:12 +0000

    Condition before permutation, still timing out

--- smaller_number.rb
+++ smaller_number.rb
@@ -1,58 +1,28 @@
-def next_smaller(n)
-  n_array = n.to_s.chars
-  return -1 if n_array.length == 1
+def next_smaller(number)
+  puts number  
+  number_array = number.to_s.chars.map(&:to_i)
+    i = number_array.length - 1
+    k = 2
+    return -1 if number_array.length == 1
+    while i >= 0 
+        if number_array[i] < number_array[i - 1]
+            number_array_copy = Array.new(number_array)
+            popped_number_array = number_array_copy.pop(k)
+            new_number = number_array_copy.join + part_permutation(popped_number_array)
+            if new_number.to_i < number && new_number.to_i.to_s.chars.length == number.to_s.chars.length
+                return new_number.to_i
+                break
+            end
+        end
+        i -= 1
+        k += 1
+    end
+    return -1
+end
 
   
-  if n_array[-1] < n_array[-2]
-    #swap the last two digits
-    n_array[-1],n_array[-2] = n_array[-2],n_array[-1]
-    return n_array.join.to_i
-
-  elsif n_array[-2] < n_array[-3]
-    #permutate the last three digits
-    #change this so that it adds all the first characters
-    first_chars = ""
-    last_three = n_array.last(3)
-    last_three_joined = last_three.join
-   
-    last_three_pas = last_three.permutation.map(&:join).sort
-    
-    last_three_final = last_three_pas[last_three_pas.index(last_three_joined)-1]
-
-    result = (first_chars + last_three_final).to_i
-    puts result
+  def part_permutation(number_array)
+    permutated_array = number_array.permutation.map(&:join).sort
+    #puts "permutated array is #{permutated_array}"
+    part_of_new_number = permutated_array[permutated_array.index(number_array.join) - 1]
   end
-end
-
-
-puts next_smaller(312)
-puts next_smaller(321)
-=begin
-  d.map!{|x| x.to_i}
-  e = d[d.index(n)-1]
-  if n > e && n.size() == e.size()
-    e
-  else
-    -1
-  end
-end
-
-74087 = ["7", "4", "0", 7 , 8]
-
-array[-1] < array[-2]
-then simply swap last two
-array[-2] < array[-3]
-then permutate last three
-array_of_integers --> [7, 4, 8, 7, 9]  #should turn to 7,4,7,8,9
-a = [7, 4]
-a = a.join
-b = [8, 7, 9]
-three_digit_n = b.join
-new_array = b.permutation.map(&:join).sort
-e = new_array[new_array.index(three_digit_n)-1].to_s
-
-result = (a+e).to_i
-
-return result
-=end
-
